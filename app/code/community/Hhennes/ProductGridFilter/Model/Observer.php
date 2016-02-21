e015a9ff2b210ec000bcb880678f8fe1778034eb
herve <nenes_fr@yahoo.fr>
2016-02-21 13:27:38 +0100

    V 0.3.0

--- app/code/community/Hhennes/ProductGridFilter/Model/Observer.php
+++ app/code/community/Hhennes/ProductGridFilter/Model/Observer.php
@@ -13,12 +13,36 @@ class Hhennes_ProductGridFilter_Model_Observer {
         $block = $observer->getEvent()->getBlock();
         if (!isset($block))
             return;
+        
+        if ( $block->getType() == 'adminhtml/catalog_category_tab_product' && Mage::getStoreConfig('system/hhennes_productgridfilter/category_status_filter') == 1) {
+                        
+            //On ajoute les nouveaux champs après le champ SKU
+                $block->addColumnAfter('status', array(
+                    'header' => Mage::helper('hhennes_productgridfilter')->__('status'),
+                    'align' => 'left',
+                    'index' => 'status',
+                    'type' => 'options',
+                    'options' => Mage::getSingleton('catalog/product_status')->getOptionArray(),
+                    'width' => '70'
+                    ),
+                    'sku');
+                
+                $block->addColumnAfter('visibility', array(
+                    'header' => Mage::helper('hhennes_productgridfilter')->__('visibility'),
+                    'align' => 'left',
+                    'index' => 'visibility',
+                    'type' => 'options',
+                    'options' => Mage::getModel('catalog/product_visibility')->getOptionArray(),
+                    'width' => '70'
+                    ),
+                    'sku');
+        }
 
         if (!Mage::getStoreConfig('system/hhennes_productgridfilter/attributes_to_display'))
             return;
 
         //Si le block correspond à la grid d'affichage des produits  
-        if ($block->getType() == 'adminhtml/catalog_product_grid') {
+        if ($block->getType() == 'adminhtml/catalog_product_grid' ) {
 
             $fields = explode(',', Mage::getStoreConfig('system/hhennes_productgridfilter/attributes_to_display'));
             
@@ -42,19 +66,32 @@ class Hhennes_ProductGridFilter_Model_Observer {
     public function onEavLoadBefore(Varien_Event_Observer $observer) {
 
         $collection = $observer->getCollection();
-
+        
         if (!isset($collection))
             return;
+        
+        //Affichage des filtres "visibilité" et "statut" dans le listing des produits des catégories
+        if (   Mage::getStoreConfig('system/hhennes_productgridfilter/category_status_filter') == 1
+                && is_a($collection, 'Mage_Catalog_Model_Resource_Product_Collection') 
+                && Mage::app()->getRequest()->getControllerName() == 'catalog_category') {
+
+            $collection->joinAttribute(
+                    'status', 'catalog_product/status', 'entity_id', null, 'inner', Mage::app()->getRequest()->getParam('store')
+            );
+            $collection->joinAttribute(
+                    'visibility', 'catalog_product/visibility', 'entity_id', null, 'inner', Mage::app()->getRequest()->getParam('store')
+            );
+        }
         
         if (!Mage::getStoreConfig('system/hhennes_productgridfilter/attributes_to_display'))
             return;
 
         //Si la collection est une collection Mage_Catalog_Model_Resource_Product_Collection on ajoute les valeurs de ces champs à la collection
         if (is_a($collection, 'Mage_Catalog_Model_Resource_Product_Collection')) {
-            
-            $fields = explode(',',Mage::getStoreConfig('system/hhennes_productgridfilter/attributes_to_display'));
-            
-            foreach ( $fields as $field)
+
+            $fields = explode(',', Mage::getStoreConfig('system/hhennes_productgridfilter/attributes_to_display'));
+
+            foreach ($fields as $field)
                 $collection->addAttributeToSelect($field); //Le champ reference peu être remplacé par l'attribut existant de votre choix 
         }
     }
